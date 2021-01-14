1eb819af32453155ea9aab37e98bd915f8e858db
mykaraoke <77389807+mykaraoke@users.noreply.github.com>
2021-01-14 10:54:15 +0630

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>စိုင်းထီးဆိုင်</title>
+</head>
+<link rel="stylesheet" href="styles.css">
+<body>
+<ul>
+  <li><span>home</span></li>
+  <li><span>products</span></li>
+  <li><span>services</span></li>
+  <li><span>contact</span></li>
+</ul>
+</body>
+</html>
